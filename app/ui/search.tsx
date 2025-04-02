73b8b3bd30d135733f133190efeced26e94e5ef0
xipholena <fake@gmail.com>
2025-04-02 10:52:02 +0300

    Chapter 15

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,16 +1,43 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 export default function Search({ placeholder }: { placeholder: string }) {
+  
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  /* Search flow step 5. add debounce */
+  const handleSearch = useDebouncedCallback((term: string) => {
+    /* Search flow step 2. Update the URL with the search params */
+    const params = new URLSearchParams(searchParams);
+    //Pagination flow step 4. user types a new search query, you want to reset the page number to 1
+    params.set('page', '1');
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+
+  }, 300)
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
         Search
       </label>
+      
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        /* Search flow step 1. Capture the user's input */
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        /* Search flow step 3. Keeping the URL and input in sync*/
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
